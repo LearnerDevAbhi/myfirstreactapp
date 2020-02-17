3b0c8d9551b2eead1ecf62cd13ac0603db1a1930
Abhishek Tiwari <55711550+tiwabhi124-node@users.noreply.github.com>
2020-02-17 21:25:54 +0530

    state
    state in react js

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+//import { Fragment } from 'react';
+//import PropTypes from 'prop-types';
 
- const Employe=(props)=>{
- return (
- <div>
-     <h1>hii Guys </h1>
- <h2>My Name is {props.Name}</h2>
-</div>
+//  const Employe=(props)=>{
+//  return (
+//  <div>
+//      <h1>hii Guys </h1>
+//  <h2>My Name is {props.Name}</h2>
+// </div>
     
- )
+//  )
+// }
+
+// class Student extends React.Component{
+//     render(){
+//     return <Fragment>
+//       <h1>Hi Guys</h1>
+//      <h2>my Name is {this.props.Name}</h2>
+//      </Fragment> 
+
+//     }
+// }
+class Employe extends React.Component{
+
+    constructor(props){
+    super(props);
+    this.state={
+        Name:this.props.Name,
+        count:this.props.count,
+    }
+    this.clickme=this.clickme.bind(this); 
+}
+clickme(){
+    let count=this.state.count;
+this.setState({count: ++count});
 }
 
-class Student extends React.Component{
     render(){
-    return <h2>my Name is {this.props.Name}</h2>
+    
+    return( <>
+    <h1>hi {this.state.Name}</h1>
+    <h1>your Employe ID {this.props.Empid}</h1>
+    <h1>Count:{this.state.count}</h1>
+      <button onClick={this.clickme}>clickme</button>
+    </>)
     }
 }
-Employe.propTypes={
-    Name:PropTypes.string,
-    }
 
-
-const Avtar=()=>{
-    return <div>
-        <Employe Name={23456}/>
-        <Student Name2="Abhishek"/>
-
-    </div>
-}
-ReactDOM.render(< Avtar/>, document.getElementById('root'));
+ReactDOM.render(<Employe Name="Abhishek" Empid={12345} count={0}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
